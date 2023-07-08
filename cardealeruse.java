5ead839ae80b5bbd8cd2605c662f00a3e7d359b1
Chandrakalavathi173 <130962067+Chandrakalavathi173@users.noreply.github.com>
2023-07-08 20:41:49 +0530

    Update cardealeruse.java

--- cardealeruse.java
+++ cardealeruse.java
@@ -1,18 +1,19 @@
 public class cardealeruse extends cardealer{
 	public static void main(String[] args) {
-		cardealeruse cdu=new cardealeruse();
+		cardealer cd=new cardealer();
 		String[] arr= new String[]{"Polo Trendline","Polo Highline","Virtus Trendline","Virtus Highline","Taigun Trendline","Taigun Highline","Taigun Topline"};
 		long[] cost=new long[]{870000,1009000,1105000,1308000,1489000,1542000,1771000};
-		cdu.getCarModel();
+		cd.getInfo();
 		for(int i=0;i<arr.length;i++)
-	  {
+	  	{
 			
-			if(arr[i].equals(cdu.carmodel))
+			if(arr[i].equals(cd.getCarmodel()))
 			{
-				cdu.getCost(arr[i],cost[i]);
+				cd.getCost(arr[i], cost[i]);
 				
 			}
 		}
 	}
+
 }
 
